1d3271a93fb6320f30bdc7d3fa896711b35901d4
Ahmed Samir Morsy <ahmedmorsy97@users.noreply.github.com>
2023-05-22 23:10:58 +0300

    init

---
+++ routes/private/api.js
@@ -0,0 +1,78 @@
+const { isEmpty } = require("lodash");
+const { v4 } = require("uuid");
+const db = require("../../connectors/db");
+const roles = require("../../constants/roles");
+const getUser = async function (req) {
+  const sessionToken = getSessionToken(req);
+  if (!sessionToken) {
+    return res.status(301).redirect("/");
+  }
+  console.log("hi",sessionToken);
+  const user = await db
+    .select("*")
+    .from("se_project.sessions")
+    .where("token", sessionToken)
+    .innerJoin(
+      "se_project.users",
+      "se_project.sessions.userid",
+      "se_project.users.id"
+    )
+    .innerJoin(
+      "se_project.roles",
+      "se_project.users.roleid",
+      "se_project.roles.id"
+    )
+   .first();
+
+  console.log("user =>", user);
+  user.isNormal = user.roleid === roles.user;
+  user.isAdmin = user.roleid === roles.admin;
+  user.isSenior = user.roleid === roles.senior;
+  console.log("user =>", user)
+  return user;
+};
+
+module.exports = function (app) {
+  // example
+  app.get("/api/v1/users", async function (req, res) {
+    try {
+      //  const user = await getUser(req);
+      const users = await db.select('*').from("se_project.users")
+        
+      return res.status(200).json(users);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not get users");
+    }
+   
+  });
+
+  app.get("/api/v1/user/:id", async function (req, res) {
+    try {
+      //  const user = await getUser(req);
+      const users = await db.select('*').from("se_project.users")
+        
+      return res.status(200).json(users);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not get users");
+    }
+   
+  });
+
+  app.put("/api/v1/password/reset", async function (req, res) {
+    try {
+      const { newPassword } = req.body;
+      if(!newPassword) {
+        throw "Password is required"
+      }
+      await db.select('*').from("se_project.users").where('id', req.userid).update('password', newPassword)
+        
+      return res.status(200).json("User password reset successfully");
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not get users");
+    }
+   
+  });
+};
